3a604b1658da5ac1ea274effe24442a26290edc0
Milagod <Milagod@DESKTOP-9K4536U>
2018-09-05 22:49:44 -0300

    Agregue clases del modelo UML al proyecto...sujeto a cambios! :3

---
+++ Entidad-Relacion/src/model/Connector.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Milagod
+ */
+public class Connector {
+    
+    private Element element1;
+    private Element element2;
+
+    public Element getElement1() {
+        return element1;
+    }
+
+    public void setElement1(Element element1) {
+        this.element1 = element1;
+    }
+
+    public Element getElement2() {
+        return element2;
+    }
+
+    public void setElement2(Element element2) {
+        this.element2 = element2;
+    }
+
+}
