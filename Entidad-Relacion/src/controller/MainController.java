87fea214e4b97ac1179916667e4d7d8e67b8de47
Alejandra <Alejandra@Ayline>
2018-11-27 22:08:48 -0300

    undo y redo ahora si que si

--- Entidad-Relacion/src/controller/MainController.java
+++ Entidad-Relacion/src/controller/MainController.java
@@ -94,7 +94,9 @@ public class MainController extends CallPop implements Initializable{
     public static ArrayList<Diagram> diagramsUndo;
     public static ArrayList<Diagram> diagramsRedo;
     public static boolean rehacer;
+    public static boolean deshacer;
     public static Diagram copy;
+    public static Diagram copy2;
     
     /**
      * Accion para cerrar la ventana.
@@ -584,8 +586,8 @@ public class MainController extends CallPop implements Initializable{
         diagramsUndo= new ArrayList<>();
         diagramsRedo= new ArrayList<>();
         rehacer=false;
-        copy();
         copy= new Diagram();
+        copy();
         showPoints = false;
         canvas.setCursor(Cursor.DEFAULT);
     }
@@ -663,41 +665,87 @@ public class MainController extends CallPop implements Initializable{
     
     @FXML
     public void undo(){
+        rehacer=false;
+        //primer deshacer no lo pesca
+        if(deshacer==false){
+            deshacer=true;
+            copy=diagram.getClone();
+            diagramsRedo.add(copy);
+            diagramsUndo.remove(diagramsUndo.size()-1);
+        }
         if(!diagramsUndo.isEmpty()){
             diagram=diagramsUndo.get(diagramsUndo.size()-1).getClone(); 
             copy=diagram.getClone();
             diagramsRedo.add(copy);
             diagramsUndo.remove(diagramsUndo.size()-1);
+            
         }
         diagram.actualizar();
         diagram.paint(canvas, showPoints);     
     }
     
     @FXML
-    public void redo(){
+    public void redo(){   
+        deshacer=false;
+        if(rehacer==false){
+            rehacer=true;
+            diagramsUndo.add(diagram);
+            diagramsRedo.remove(diagramsRedo.size()-1);
+        }
         if(!diagramsRedo.isEmpty()){
-            System.out.println("entre al rehacer");
             rehacer=true;
             diagram=diagramsRedo.get(diagramsRedo.size()-1).getClone();
             diagramsUndo.add(diagram);
             diagramsRedo.remove(diagramsRedo.size()-1);
-          
         }
         diagram.actualizar();
         diagram.paint(canvas, showPoints);
         
     }
     
-    
     public static void copy(){
+        deshacer=false;
         if(rehacer==true){
             System.out.println("entre al false");
             rehacer=false;
             diagramsRedo.clear();
-        }      
+        } 
         copy = diagram.getClone();
         diagramsUndo.add(copy);
-        System.out.println("d" + diagramsUndo.size());
-        
+        System.out.println("d" + diagramsUndo.size());        
     }
+    /*
+    @FXML
+    public void undo(){
+        if(deshacer==false){
+            deshacer=true;
+            copy=diagram.getClone();
+            diagramsRedo.add(copy);
+            diagramsUndo.remove(diagramsUndo.size()-1);
+        }
+        if(!diagramsUndo.isEmpty()){
+            diagram=diagramsUndo.get(diagramsUndo.size()-1).getClone(); 
+            copy=diagram.getClone();
+            diagramsRedo.add(copy);
+            diagramsUndo.remove(diagramsUndo.size()-1); 
+        }
+        else{
+            diagram= new Diagram();
+        }
+        diagram.actualizar();
+        diagram.paint(canvas, showPoints);     
+    }
+    
+    @FXML
+    public void redo(){   
+        deshacer=false;
+        if(!diagramsRedo.isEmpty()){
+            rehacer=true;
+            diagram=diagramsRedo.get(diagramsRedo.size()-1).getClone();
+            diagramsUndo.add(diagram);
+            diagramsRedo.remove(diagramsRedo.size()-1);
+        }
+        diagram.actualizar();
+        diagram.paint(canvas, showPoints);
+    }*/
 }
